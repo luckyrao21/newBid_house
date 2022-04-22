58958616ea4bba172388de6df7875c1ee977c00b
Narendra Pipliwal <rajkasotiya26@gmail.com>
2022-04-23 00:51:45 +0530

    Raj This Side

--- controller/bid.controller.js
+++ controller/bid.controller.js
@@ -3,13 +3,13 @@ const BID = require('../model/bid.model')
 const { request } = require("express");
 const bidModel = require("../model/bid.model");
 
-exports.addBid = async (request,response) => {
-  
+exports.addBid = async(request, response) => {
+
     let bidModel = await BID.findOne({
-        productId:request.body.productId
+        productId: request.body.productId
     })
-  
-    if (!bidModel){
+
+    if (!bidModel) {
         bidModel = new BID();
         bidModel.productId = request.body.productId;
     }
@@ -17,40 +17,39 @@ exports.addBid = async (request,response) => {
         buyersId: request.body.buyersId,
         priceValue: request.body.priceValue,
     });
-    bidModel.save().then(result=>{
-       if(result) 
-          return response.status(200).json({data: result, succes: "Bid Added Successfully"})
-       else
-          return response.status(200).json({data: result, succes: "Bid Not Added Successfully"})
-    }).catch(err=>{
-        return response.status(500).json({error:"oops something went wrong"})
+    bidModel.save().then(result => {
+        if (result)
+            return response.status(200).json({ data: result, succes: "Bid Added Successfully" })
+    }).catch(err => {
+        return response.status(500).json({ error: "oops something went wrong" })
     })
 }
 
-exports.viewBidList = async (request,response) => {
-        
-    bidModel.find().populate('creator.buyersId').populate('productId').then(result=>{
-        return response.status(200).json(result)
-    }).catch(err=>{
+exports.viewBidList = async(request, response) => {
+
+    bidModel.find().populate('creator.buyersId').populate('productId').then(result => {
+        if (result)
+            return response.status(200).json(result)
+        else
+            return response.status(200).json({ messege: "Not Found" })
+    }).catch(err => {
         console.log(err);
-        return response.status(500).json({err:"oops something went wrong"})
+        return response.status(500).json({ err: "oops something went wrong" })
     })
 }
 
-exports.viewOneProductBid = async (request,response) => {
+exports.viewOneProductBid = async(request, response) => {
     bidModel.findOne({
-        productId:request.body.productId
-    }).populate('creator.buyersId').populate('productId')
-    .then(result=>{
-       if(result){
-         result.creator.sort((a,b)=> {return b.priceValue -a.priceValue})
-         return response.status(200).json(result)
-       }
-       else
-         return response.status(200).json({messege: "Result Not Found..."})
-    }).catch(err=>{
-        console.log(err);
-        return response.status(500).json({err:"oops something went wrong"})
-    })
+            productId: request.body.productId
+        }).populate('creator.buyersId').populate('productId')
+        .then(result => {
+            if (result) {
+                result.creator.sort((a, b) => { return b.priceValue - a.priceValue })
+                return response.status(200).json(result)
+            } else
+                return response.status(200).json({ messege: "Result Not Found..." })
+        }).catch(err => {
+            console.log(err);
+            return response.status(500).json({ err: "oops something went wrong" })
+        })
 }
-
