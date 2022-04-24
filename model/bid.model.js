41c843a0ad69adf192809e8529b3845ffa55d24b
Narendra Pipliwal <rajkasotiya26@gmail.com>
2022-04-25 04:48:17 +0530

    Raj This Side

--- model/bid.model.js
+++ model/bid.model.js
@@ -2,30 +2,28 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const bidSchema = new mongoose.Schema({
-    creator: [
-        {
-            buyersId: {
-                type: schema.Types.ObjectId,
-                ref: 'customer'
-            },
-            priceValue: {
-                type: Number,
-                required: true,
-            },
-            isApproved: {
-                type: Boolean,
-                default: false
-            }
-        }
-    ],
+    creator: [{
+        buyersId: {
+            type: schema.Types.ObjectId,
+            ref: "customer",
+        },
+        priceValue: {
+            type: Number,
+            required: true,
+        },
+        isApproved: {
+            type: Boolean,
+            default: false,
+        },
+    }, ],
     productId: {
         type: schema.Types.ObjectId,
-        ref: 'product'
+        ref: "product",
     },
     date: {
         type: Date,
-        default: new Date().now
-    }
-})
+        default: new Date().now,
+    },
+});
 
 module.exports = mongoose.model("bid", bidSchema);
